809edcc639d025b5d4bee597027788ad053e7112
EliasBallen <@EliasBallen>
2023-11-16 13:29:10 -0500

    mount body parser with app.use

--- myApp.js
+++ myApp.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 let express = require('express');
+let bodyParser = require('body-parser');
 let app = express();
 
-
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 
 /*
 app.use((req,res,next)=>{
