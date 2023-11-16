ae4de5b274db626b98919207847e3cf9546cf820
EliasBallen <@EliasBallen>
2023-11-15 22:32:56 -0500

    add middleware function in app.use for any request

--- myApp.js
+++ myApp.js
@@ -5,7 +5,10 @@ let app = express();
 
 
 
-
+app.use((req,res,next)=>{
+    console.log(`${req.method} ${req.path} - ${req.ip}`)
+    next();
+})
 
 app.get("/",(req,res)=>{
     //res.send("Hello Express");
