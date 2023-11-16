237a4f3e7118fd038943ff3f26f4ace6aa119d42
EliasBallen <@EliasBallen>
2023-11-15 20:44:42 -0500

    add the express.static method to the App.use to serve static files into the page from the public directory

--- myApp.js
+++ myApp.js
@@ -4,12 +4,14 @@ let app = express();
 
 
 
+
+
 app.get("/",(req,res)=>{
     //res.send("Hello Express");
     res.sendFile(__dirname+"/views/index.html");
 });
 
-
+app.use("/public",express.static(__dirname+"/public"))
 
 
 
